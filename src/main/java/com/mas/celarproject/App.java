e2d83c520e4565c06c217b6ba5467592d642d8df
DanRobS <roberttsoumbou@gmail.com>
2019-12-18 03:33:22 +0100

    version 1 push

--- src/main/java/com/mas/celarproject/App.java
+++ src/main/java/com/mas/celarproject/App.java
@@ -1,25 +1,12 @@
 package com.mas.celarproject;
 
-import java.io.BufferedReader;
-import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
-import java.io.FileReader;
 import java.io.IOException;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.List;
-
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
-import javax.xml.transform.Transformer;
-import javax.xml.transform.TransformerConfigurationException;
-import javax.xml.transform.TransformerException;
-import javax.xml.transform.TransformerFactory;
-import javax.xml.transform.dom.DOMSource;
-import javax.xml.transform.stream.StreamResult;
 
 import org.jdom2.output.Format;
 import org.jdom2.output.XMLOutputter;
@@ -29,11 +16,11 @@ import org.jdom2.Element;
 public class App 
 {
 	//Creates the <agents> tag
-	static public Element createAgentTag() {
+	static public Element createAgentTag(int nb) {
 		int i=1;
 		Element agents = new Element("agents");
 		try {
-			List<String> fileLines = Files.readAllLines(Paths.get("var.txt"), Charset.defaultCharset());
+			List<String> fileLines = Files.readAllLines(Paths.get("CELAR/scen0"+nb+"/VAR.TXT"), Charset.defaultCharset());
             
             agents.setAttribute("nbAgents",""+fileLines.size());
             for ( String line : fileLines) {
@@ -54,19 +41,20 @@ public class App
 	}
 	
 	//Creates the <domain> tag
-	static public Element createDomainTag() {
+	static public Element createDomainTag(int nb) {
 		Element domains = new Element("domains");
 		try {
-			List<String> fileLines = Files.readAllLines(Paths.get("dom.txt"), Charset.defaultCharset());
+			List<String> fileLines = Files.readAllLines(Paths.get("CELAR/scen0"+nb+"/DOM.TXT"), Charset.defaultCharset());
 			fileLines.remove(0);
 			
 			domains.setAttribute("nbDomains",""+fileLines.size());
 			for(String line: fileLines) {
-				String[] separator = line.trim().split("  ");
+				String[] separator = line.trim().split(" ");
 				Element domain = new Element("domain");
 				domain.setAttribute("name",separator[0]);
-				domain.setAttribute("nbValues",separator[1]);
-				domain.setText(separator[2]+"  "+separator[fileLines.size()+1]);
+				domain.setAttribute("nbValues",separator[2]);
+	//System.out.println(separator[2]);			
+				domain.setText(separator[4]+"  "+separator[separator.length-1]);
 				domains.addContent(domain);
 				//System.out.println(separator[fileLines.size()+1]);
 			}
@@ -80,10 +68,10 @@ public class App
 	}
 	
 	//Creates the <variables> tag
-	static public Element createVariableTag() {
+	static public Element createVariableTag(int nb) {
 		Element variables = new Element("variables");
 		try {
-			List<String> fileLines = Files.readAllLines(Paths.get("var.txt"), Charset.defaultCharset());
+			List<String> fileLines = Files.readAllLines(Paths.get("CELAR/scen0"+nb+"/VAR.TXT"), Charset.defaultCharset());
 			
 			variables.setAttribute("nbVariables",""+fileLines.size());
 			for(String line: fileLines) {
@@ -102,11 +90,11 @@ public class App
 	}
 	
 	//Creates the <constraints> tag
-	static public Element createConstraintTag() {
+	static public Element createConstraintTag(int nb) {
 		Element constraints = new Element("constraints");
 		
 		try {
-			List<String> fileLines = Files.readAllLines(Paths.get("ctr.txt"), Charset.defaultCharset());
+			List<String> fileLines = Files.readAllLines(Paths.get("CELAR/scen0"+nb+"/CTR.TXT"), Charset.defaultCharset());
 			int i=0;
 			constraints.setAttribute("nbConstraints",""+fileLines.size());
 			for(String line : fileLines) {
@@ -182,7 +170,7 @@ public class App
 	}
 	
 	//Generates the file problem.xml to use with FRODO
-	static public void generateProblem() {
+	static public void generateProblem(int nb) {
 		
         try {
 			Element instance = new Element("instance");
@@ -195,11 +183,11 @@ public class App
 			presentation.setAttribute("maximizing","false");
 			
 			instance.addContent(presentation);
-			instance.addContent(createAgentTag());
-			instance.addContent(createDomainTag());
-			instance.addContent(createVariableTag());
+			instance.addContent(createAgentTag(nb));
+			instance.addContent(createDomainTag(nb));
+			instance.addContent(createVariableTag(nb));
 			instance.addContent(createPredicatesTag());
-			instance.addContent(createConstraintTag());
+			instance.addContent(createConstraintTag(nb));
 
 			XMLOutputter xmlOutputter = new XMLOutputter(Format.getPrettyFormat());
             xmlOutputter.output(document, new FileOutputStream("problem.xml"));
@@ -215,6 +203,6 @@ public class App
 	
     public static void main( String[] args )
     {
-        generateProblem();
+        generateProblem(1);
     }
 }
